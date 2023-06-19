373b72c8f1481aa1f3a1aff654453d723576dc0e
Akshay Dodkade <akshay.d@westagilelabs.com>
2023-06-19 14:27:58 +0530

    exceptional handling - added exceptional handling and improvised start log and end log time calculations

--- fair-billing.py
+++ fair-billing.py
@@ -35,72 +35,82 @@ def log_validation(log):
 
     return True
 
-# get file
-log_file = sys.argv[1:][0]
-with open(log_file, 'r+') as file:
-    lines = [line.rstrip() for line in file]
-
-
-# process file data
-for idx, line in enumerate(lines):
-    time_spend = 0
-    line_data = line.split()
-
-    # get start and end log entry
-    if idx == 0:
-        log_start = line_data[0]
-    if idx == len(lines)-1:
-        log_end = line_data[0]
-
-    # data validation
-    if log_validation(line) == False:
-        continue
-
+try:
+    # get file
+    if (len(sys.argv[1:]) < 1):
+        raise Exception("Please add log file to continue")
     
-    if not any(data['user'] == line_data[1] for data in log_report):
-        active_sessions = []
-        if line_data[2] == 'End':
-            time_spend += get_time(log_start, line_data[0])
-        else:
-            active_sessions = [
-                {
-                    'time': line_data[0],
-                    'type': line_data[2],
-                }
-            ]
-        log_report.append({
-            'user': line_data[1],
-            'sessions': 1,
-            'time_spend': time_spend,
-            'active_sessions': active_sessions
-          })
-    else:
-        log_idx = log_report.index(next(filter(lambda n: n.get('user') == line_data[1], log_report)))
-        if line_data[2] == 'Start':
-            log_report[log_idx]['sessions'] += 1
-            log_report[log_idx]['active_sessions'].append({
-                'time': line_data[0],
-                'type': line_data[2],
+    log_file = sys.argv[1:][0]
+    with open(log_file, 'r+') as file:
+        lines = [line.rstrip() for line in file]
+
+
+    # process file data
+    for idx, line in enumerate(lines):
+        time_spend = 0
+        line_data = line.split()
+
+        # data validation
+        if log_validation(line) == False:
+            continue
+
+        # get start time
+        if (log_start == ""):
+            log_start = line_data[0]
+
+        # update end time
+        log_end = line_data[0]
+        
+        if not any(data['user'] == line_data[1] for data in log_report):
+            active_sessions = []
+            if line_data[2] == 'End':
+                time_spend += get_time(log_start, line_data[0])
+            else:
+                active_sessions = [
+                    {
+                        'time': line_data[0],
+                        'type': line_data[2],
+                    }
+                ]
+            log_report.append({
+                'user': line_data[1],
+                'sessions': 1,
+                'time_spend': time_spend,
+                'active_sessions': active_sessions
             })
         else:
-            if len(log_report[log_idx]['active_sessions']) > 0:
-                # get first session and calculate difference
-                time_spend = get_time(log_report[log_idx]['active_sessions'][0]['time'], line_data[0])
-                log_report[log_idx]['time_spend'] += time_spend
-                log_report[log_idx]['active_sessions'].pop(0)
-            else:
-                time_spend = get_time(log_start, line_data[0])
+            log_idx = log_report.index(next(filter(lambda n: n.get('user') == line_data[1], log_report)))
+            if line_data[2] == 'Start':
                 log_report[log_idx]['sessions'] += 1
-                log_report[log_idx]['time_spend'] += time_spend
+                log_report[log_idx]['active_sessions'].append({
+                    'time': line_data[0],
+                    'type': line_data[2],
+                })
+            else:
+                if len(log_report[log_idx]['active_sessions']) > 0:
+                    # get first session and calculate difference
+                    time_spend = get_time(log_report[log_idx]['active_sessions'][0]['time'], line_data[0])
+                    log_report[log_idx]['time_spend'] += time_spend
+                    log_report[log_idx]['active_sessions'].pop(0)
+                else:
+                    time_spend = get_time(log_start, line_data[0])
+                    log_report[log_idx]['sessions'] += 1
+                    log_report[log_idx]['time_spend'] += time_spend
 
 
-# log report final cleanup
-for record in log_report:
-    if len(record['active_sessions']) > 0:
-        time_spend = 0
-        for idx, log in enumerate(record['active_sessions']):
-            time_spend = get_time(record['active_sessions'][idx]['time'], log_end)
-            record['time_spend'] += time_spend
-        record['active_sessions'] = []
-    
-    print(f"{record['user']} {record['sessions']} {record['time_spend']}")
+    # log report final cleanup
+    for record in log_report:
+        if len(record['active_sessions']) > 0:
+            time_spend = 0
+            for idx, log in enumerate(record['active_sessions']):
+                time_spend = get_time(record['active_sessions'][idx]['time'], log_end)
+                record['time_spend'] += time_spend
+            record['active_sessions'] = []
+        
+        print(f"{record['user']} {record['sessions']} {record['time_spend']}")
+
+except FileNotFoundError:
+    print(f"Error: Please add log file")
+
+except Exception as e:
+    print(f"Error: {e}")
